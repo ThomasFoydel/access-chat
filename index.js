8e3aab3f200e1babe1cf32ccac9ac5b1440b84e8
ThomasFoydel <thomasjfoydel@gmail.com>
2021-03-07 19:50:01 -0600

    removed body parser, added backend socket initial connection

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const app = express();
 const apiRoutes = require('./routes/api');
+const socketio = require('socket.io');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const port = process.env.PORT || 8000;
 const mongoURI =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGO_URI
@@ -33,8 +33,31 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is up on port ${port}!`);
+    const expressServer = app.listen(process.env.PORT || 8000);
+    const io = socketio(expressServer);
+
+    io.on('connection', (socket) => {
+      const token = socket.handshake.query.token;
+      if (!token) {
+        console.log('no token auth denied');
+      } else {
+        try {
+          const decoded = jwt.verify(token, process.env.SECRET);
+          let currentTime = Date.now() / 1000;
+          if (decoded.exp < currentTime) {
+          } else {
+            let { userId } = decoded.tokenUser;
+            const currentUser = {
+              userId: userId,
+              socketId: socket.id,
+            };
+            users = users.filter((user) => user.userId !== userId);
+            users.push(currentUser);
+          }
+        } catch (err) {
+          console.log('err: ', err);
+        }
+      }
     });
   });
 
